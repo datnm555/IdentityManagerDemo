ce52ae9f37f79ba71d4eab81d45df59ecef78291
DESKTOP-P944O7J\datnguyen <mydatng@gmail.com>
2022-11-05 21:05:44 +0700

    Add project files.

---
+++ IdentityManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using IdentityManager.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityManager.Data
+{
+    public class ApplicationDbContext :  IdentityDbContext
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="options"></param>
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        
+        public  DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
